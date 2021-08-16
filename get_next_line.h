f7f11dede58eb612bc020683ecd7bc57ea44bbdc
renato <felicio95.jr@gmail.com>
2021-08-16 17:24:22 -0300

    :wrench: Fixing norm errors

--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,21 @@
 /*   By: rfelicio <rfelicio@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/15 19:41:15 by rfelicio          #+#    #+#             */
-/*   Updated: 2021/08/15 21:56:41 by rfelicio         ###   ########.fr       */
+/*   Updated: 2021/08/16 17:23:44 by rfelicio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# include "unistd.h"
+# include "stdlib.h"
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1000
+# endif
+
+# define NEW_LINE "\n"
 
 char	*get_next_line(int fd);
 
