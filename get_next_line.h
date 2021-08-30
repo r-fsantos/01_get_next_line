d83035ba19bbdc42e687b29accd4d803c003d742
renato <felicio95.jr@gmail.com>
2021-08-30 14:46:08 -0300

    :coffin: Removing no longer needed functions declaration on get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: rfelicio <rfelicio@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/15 19:41:15 by rfelicio          #+#    #+#             */
-/*   Updated: 2021/08/21 20:14:24 by rfelicio         ###   ########.fr       */
+/*   Updated: 2021/08/30 14:44:46 by rfelicio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,16 +36,9 @@ char	*ft_get_line(char **stream);
 /*
 ** Many thanks to the helpers && utils, writen during the libft project.
 */
-
-char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strchr(const char *s, int c);
-char	*ft_strdup(const char *str);
-size_t	ft_strlen(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
-void	*ft_calloc(size_t count, size_t size);
-void	*ft_memset(void *b, int c, size_t len);
-void	ft_bzero(void *s, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
 
 #endif
